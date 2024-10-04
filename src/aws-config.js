645e0fba73031273c217106ee2efd7aff3802757
Muhammad Afandi bin Azmi <afandi_army@live.com>
2024-10-04 09:34:22 +0800

    new update

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,6 +1,6 @@
 // aws-config.js
 export const AWS_REGION = "ap-southeast-1";
 export const IDENTITY_POOL_ID =
-  "ap-southeast-1:XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
+  "XXXXXXXXXXXXX:3211b668-d076-4a72-a377-1933eaa868f2";
 export const AWS_IOT_ENDPOINT =
-  "XXXXXXXXXXXXXXX-ats.iot.ap-southeast-1.amazonaws.com";
+  "XXXXXXXXXXXX-ats.iot.ap-southeast-1.amazonaws.com";
